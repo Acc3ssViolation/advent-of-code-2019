ee495ddd3407e9b26d34826534d1370da59c02f9
Acc3ss Violation <wrdbrookhuis@gmail.com>
2024-03-13 20:41:51 +0100

    Add project files.

---
+++ Advent/PathManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Runtime.InteropServices;
+
+namespace Advent
+{
+    internal static class PathManager
+    {
+#if DEBUG
+        private static string BaseDir
+        {
+            get
+            {
+                if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+                    return "D:/Projects/advent-of-code-2019/Advent/";
+                else
+                    return AppContext.BaseDirectory;
+            }
+        }
+#else
+        private static readonly string BaseDir = AppContext.BaseDirectory;
+#endif
+
+        public static readonly string DataDirectory = Path.Combine(BaseDir, "Data");
+    }
+}
